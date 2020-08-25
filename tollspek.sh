bee11a0241dca7196881274aafcc56f62c1f2200
anitaanitaa <69967765+anitaanitaa@users.noreply.github.com>
2020-08-25 19:49:55 +0800

    Create tollspek.sh

---
+++ tollspek.sh
@@ -0,0 +1,159 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+echo
+figlet -f slant  "ANISTA JAS"|lolcat
+
+echo $ku" WELOCOME "
+echo $ij"/  ^   ^  \ "
+echo $me"\▪■  ?  ■▪/ "
+echo $me" \ ■   ■ /  "
+echo $ku"- \  -  /   "
+echo $ku"×  \ ¤ /    "
+echo $ku"🐒🐒🐒🐒🐒🐒🐒"
+echo $ku"👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹"
+echo $ku"===================================="
+echo $ku"👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹👹"
+echo $ku"   #     ===================================="
+echo $ku"   #   >> AUTHOR :"ANDIELIS"$pu"
+echo
+echo $ku"   #   >> GITHUB :"GITHUB.COM ANITAANITA"$pu"
+echo
+echo $ku"   #   >> YOUTUBE:"ANITA JAS"$pu"
+echo $ku"   #    🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍"
+echo $ku"   #  >> ===================================="
+echo $ku"   #  >> 🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍🌍"
+
+echo $ku" PILIH MENU"
+
+echo $ku"   ( ∆ ___ ∆  )"
+sleep 1
+echo $ku" ₩₩₩₩₩₩₩₩₩₩₩₩₩₩₩₩₩"
+echo $ku"---_\\\\\////---"
+echo $ku" ______∆∆∆______"
+echo $ku" (((((  ●  )))))"
+sleep 1
+echo $pr"TOOLS"$ku " INDONESIA"
+sleep 1
+echo $me"|"$me" 1"$me".  TOLS PISHING"
+sleep 1
+echo $me"|"$me" 2"$me".  SPAM SMS"
+sleep 1
+echo $me"|"$me" 3"$me". TOLS SPAM (WA)"
+sleep 1
+echo $me"|"$me" 4"$me".  PHISING FB"
+sleep 1
+echo $me"|"$me" 5"$me".  VIRUS PERUSAK"
+sleep 1
+echo $me"|"$me" 6"$me".  OSIF SCAN EMAIL FB"
+sleep 1
+echo $me"|"$me" 7"$me".  HACK FB MBF"
+sleep 1
+echo $cy"|"$cy" 8"$cy".  INSTALL BAHAN GAYS"
+read -p " [?] pilih : " pil :
+# batas gan
+if [ $pil = 1 ]
+then
+git clone https://github.com/InYourG00D1/scPH1S1N9
+ls
+cd scPH1S1N9
+ls
+sh PhisingV2.sh
+fi
+# batas gan
+if [ $pil = 2 ]
+then
+git clone https://github.com/BladeKnife/2otp
+ls
+cd 2otp
+ls
+python spam.py
+fi
+# batas gan
+if [ $pil = 3 ]
+then
+git clone https://github.com/hekelpro/spammer
+ls
+cd spammer
+ls
+python2 spammer.py
+fi
+# batas gan
+if [ $pil = 4 ]
+then
+git clone https://github.com/samyoyo/weeman
+ls
+cd weeman
+ls
+python2 weeman.py
+fi
+# batas gan
+if [ $pil = 5 ]
+then
+git clone https://github.com/saydog/Virus-droid-creator
+ls
+cd Virus-droid-creator
+ls
+python2 dog.py
+fi
+if [ $pil = 6]
+then
+git clone https://github.com/storiku/osif
+ls
+cd osif
+ls
+python2 osif.py
+fi
+if [ $pil = 7 ]
+then
+git clone https://github.com/pirmansx/MBF
+ls
+cd MBF
+ls
+python2 MBF.py
+fi
+if [ $pil = 8 ]
+then
+apt update && apt upgrade
+apt install python2
+pip install --upgrade pip
+pip2 install --upgrade pip
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip install requests
+pip install requests mechanize
+pip2 install bs4
+pip2 install beautifulsoup
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant "B E R H A S I L"|lolcat                                                             
+fi
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $ku" T H A N K "
+sleep2
+echo $cy"TOLS AMAN"
+sleep 2
+echo $cy"SELAMAT "
+sleep 2
+echo $cy"MAKASIH TELAH MENGGUNAKAN TOLS KAMI"
+ exit
+fi
